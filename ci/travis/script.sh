b4530b3988137672a430420e1825193606cb74b4
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-01-18 00:00:59 +0100

    Run tests in both multithreaded and single threaded mode

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -7,7 +7,9 @@
 if cargo tarpaulin -V >/dev/null 2>&1; then
 	log Measuring code coverage with Tarpaulin
 	cargo tarpaulin -v --release --out Xml --ciserver travis-ci
+        cargo tarpaulin --no-default-features -v --release --out Xml --ciserver travis-ci
 else
 	log Testing code without coverage
 	cargo test --release
+        cargo test --no-default-features -v --release --out Xml --ciserver travis-ci
 fi
